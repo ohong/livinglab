c5abc2cf95acc7f7d6dcbf8e2fbbfb52aa439c57
Oscar Hong <oscar.hong2015@gmail.com>
2017-04-29 22:47:34 -0400

    updated views with bootstrap classes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   # function to validate Full Name, invoked before any devise_controller action
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) << :fullname
-      devise_parameter_sanitizer.for(:account_update) << :fullname
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:fullname])
     end
 end
