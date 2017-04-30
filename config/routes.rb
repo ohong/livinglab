af873a4652ed35b458d0cd0c730f1261b247ed5f
Oscar Hong <oscar.hong2015@gmail.com>
2017-04-30 18:10:33 -0400

    created user profile page & some sass styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  devise_for :users
+  devise_for  :users,
+              :path => '',
+              :path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'}
+
+  # only want to create url path for showing users only
+  resources :users, only: [:show]
 end
