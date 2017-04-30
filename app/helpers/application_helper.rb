38e5fbc87d57ad6bbb247e335b672bd249a1716e
Oscar Hong <oscar.hong2015@gmail.com>
2017-04-29 23:33:31 -0400

    implemented profile pic in navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  # gets the url for the user's gravatar(i.e. globally recognized avatar)
+  def profile_pic_url(user)
+    # ensure valid ID using MD5 encoding
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
+  end
 end
