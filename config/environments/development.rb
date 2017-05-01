2bfd2b40bf2d33fa3a5545576c78ada14755c8ea
Oscar Hong <oscar.hong2015@gmail.com>
2017-05-01 19:03:15 -0400

    changed dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,17 +30,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  # config.action_mailer.delivery_method = :smtp
-  
-  # config.action_mailer.smtp_settings = {
-  #   address: 'smtp.sendgrid.net',
-  #   port: 587,
-  #   enable_starttls_auto: true,
-  #   user_name: 'ohong',
-  #   password: 'M$u96y9j%s',
-  #   authentication: 'plain'
-  # }
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
