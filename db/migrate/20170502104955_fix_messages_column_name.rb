ababafa6ccdb4c6594038b130826518e608a0be8
Oscar Hong <oscar.hong2015@gmail.com>
2017-05-02 13:55:35 -0400

    private_pub production setup

--- db/migrate/20170502104955_fix_messages_column_name.rb
+++ db/migrate/20170502104955_fix_messages_column_name.rb
@@ -2,6 +2,5 @@ class FixMessagesColumnName < ActiveRecord::Migration[5.0]
   def change
     rename_column :messages, :conversation_references, :conversation_id
     change_column :messages, :conversation_id, 'integer USING CAST(conversation_id AS integer)'
-
   end
 end
