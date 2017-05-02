1db48d4268e075cd66c0358a9c1af3f474000097
Oscar Hong <oscar.hong2015@gmail.com>
2017-05-02 13:14:38 -0400

    fixing migration

--- db/migrate/20170502003143_change_accomodate_type_in_rooms.rb
+++ db/migrate/20170502003143_change_accomodate_type_in_rooms.rb
@@ -1,6 +1,6 @@
 class ChangeAccomodateTypeInRooms < ActiveRecord::Migration[5.0]
   def change
-    change_column :rooms, :accommodate, :integer
+    change_column :rooms, :accommodate, 'integer USING CAST(accommodate AS integer)'
     remove_column :rooms, :integer
   end
 end
