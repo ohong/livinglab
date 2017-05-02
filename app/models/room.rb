872f52aed85739be6a907eda0183a70c4e04b147
Oscar Hong <oscar.hong2015@gmail.com>
2017-05-02 15:34:44 -0400

    cascading destroy

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ApplicationRecord
   belongs_to :user
-  has_many :photos
+  has_many :photos, dependent: :destroy
 
   # get lat and long w/ address
   geocoded_by :address, :latitude  => :lat, :longitude => :long #ActiveRecord
